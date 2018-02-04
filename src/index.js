4513c7896cc83ae3c103bc91d8e1c2decb5260bf
Gianfranco Manganiello <gianfrancomanganiello@hotmail.com>
2018-02-04 11:18:30 -0400

    feat(rest-api): cancel payment

--- src/index.js
+++ src/index.js
@@ -104,4 +104,20 @@ class InstapagoRestClient {
             }
         })
     }
+
+    cancelPayment (options) {
+        return new Promise((resolve, reject) => {
+            try {
+                this._prepareRequest('POST', 'payment', options, (response, data) => {
+                    if (response.statusCode >= 400 && status < 600) {
+                        reject(JSON.parse(data))
+                    } else {
+                        resolve(JSON.parse(data))
+                    }
+                }).end()
+            } catch (err) {
+                reject(err)
+            }
+        })
+    }
 }
